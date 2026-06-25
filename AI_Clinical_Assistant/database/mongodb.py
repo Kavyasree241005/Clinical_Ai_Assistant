3602a954bb3a15030b84204a6f73f3e4ddb3df5c
Kavyasree241005 <kavyasreesreevani@gmail.com>
2026-06-25 11:36:44 +0530

    Use MongoDB Atlas

--- AI_Clinical_Assistant/database/mongodb.py
+++ AI_Clinical_Assistant/database/mongodb.py
@@ -1,17 +1,14 @@
 from pymongo import MongoClient
+import streamlit as st
 
 client = MongoClient(
-    "mongodb://localhost:27017"
+    st.secrets["MONGODB_URI"]
 )
 
 db = client["clinical_ai_db"]
 
 doctors = db["doctors"]
-
 patients = db["patients"]
-
 consultations = db["consultations"]
-
 reports = db["reports"]
-
 voice_embeddings = db["voice_embeddings"]
